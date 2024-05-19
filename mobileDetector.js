195307de6fc79fb687d9cc53ced2eb519048a02b
nexondevfromvietnam <khorryvfahnifa@tmt.vitik.edu.vn>
2024-05-19 16:22:21 +0700

    Update mobileDetector.js
    update for mobile users

--- mobileDetector.js
+++ mobileDetector.js
@@ -3,6 +3,6 @@ function DetectTheThing() {
     if (uagent.search("iphone") > -1 || uagent.search("ipad") > -1
         || uagent.search("android") > -1 || uagent.search("blackberry") > -1
         || uagent.search("webos") > -1){
-                  window.location.href ="./mobile";
+                  window.location.href ="https://mobile.o2production.ndqm.site";
         }}
 DetectTheThing(); //start detector
